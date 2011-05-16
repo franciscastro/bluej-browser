4589a222aa28dc78d2c9e9fdf8932c37fb55d012
Thomas Dy <thatsmydoing@gmail.com>
2011-05-16 21:27:29 +0800

    Cleaned up the views

--- protected/views/invocationSession/view.php
+++ protected/views/invocationSession/view.php
@@ -3,14 +3,6 @@ $this->breadcrumbs=array(
 	'Invocation Sessions'=>array('index'),
 	$model->id,
 );
-
-$this->menu=array(
-	array('label'=>'List InvocationSession', 'url'=>array('index')),
-	array('label'=>'Create InvocationSession', 'url'=>array('create')),
-	array('label'=>'Update InvocationSession', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete InvocationSession', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage InvocationSession', 'url'=>array('admin')),
-);
 ?>
 
 <h1>View InvocationSession #<?php echo $model->id; ?></h1>
